5ca3c894b3cdd5547baa28622f7d72596552b125
MACARENA TABOADA <lean.louis1997@gmail.com>
2025-01-07 21:41:13 -0300

    creacion de navbar y estilo de pagina

--- front/gecse/src/App.jsx
+++ front/gecse/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/navBar/NavBar'
 
 const App = () => {
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary text-center">¡Hola, GECSE!</h1>
-      <p className="text-muted text-center">Bootstrap está funcionando correctamente 🚀</p>
+    <div>
+    <NavBar/>
     </div>
   );
 };
